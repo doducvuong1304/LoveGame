f64d32af4b3b756323a770f0985590a6dc2ed164
King Bean <doducvuong1304@gmail.com>
2021-11-25 22:00:35 +0700

    3st commit

--- LoveGame/game.py
+++ LoveGame/game.py
@@ -14,37 +14,37 @@ screen = pygame.display.set_mode((800,600))
 background = pygame.image.load('background.png')
 
 # you
-haiImg = []
-haiX = []
-haiY = []
-haiX_change = []
-haiY_change = []
-num_of_hai = 5
+vuongImg = []
+vuongX = []
+vuongY = []
+vuongX_change = []
+vuongY_change = []
+num_of_vuong = 5
 
-for i in range(num_of_hai):
-    haiImg.append(pygame.image.load('vuong.png'))
-    haiX.append(random.randint(0,735))
-    haiY.append(random.randint(20,150))
-    haiX_change.append(0.2)
-    haiY_change.append(20)
+for i in range(num_of_vuong):
+    vuongImg.append(pygame.image.load('vuong.png'))
+    vuongX.append(random.randint(0,735))
+    vuongY.append(random.randint(20,150))
+    vuongX_change.append(0.2)
+    vuongY_change.append(20)
 
-    def hai(x,y,i):
-        screen.blit(haiImg[i],(x,y))
+    def vuong(x,y,i):
+        screen.blit(vuongImg[i],(x,y))
 
 # your girl friend
-nganImg = pygame.image.load('thuy.png')
-nganX = 380
-nganY = 500
-nganX_change = 0
-nganY_change = 0
+thuyImg = pygame.image.load('thuy.png')
+thuyX = 380
+thuyY = 500
+thuyX_change = 0
+thuyY_change = 0
 
-def ngan(x,y):
-    screen.blit(nganImg,(x,y))
+def thuy(x,y):
+    screen.blit(thuyImg,(x,y))
 
 # heart
 heartImg = pygame.image.load('heart.png')
 heartX = 0
-heartY = nganY
+heartY = thuyY
 heartX_change = 0
 heartY_change = 0.4
 
@@ -61,8 +61,8 @@ icon = pygame.image.load('heart.png')
 pygame.display.set_icon(icon)
 
 # check colision
-def iscollision(haiX,haiY,heartX,heartY):
-    distance = math.sqrt(math.pow(haiX - heartX,2)+math.pow(haiY - heartY,2))
+def iscollision(vuongX,vuongY,heartX,heartY):
+    distance = math.sqrt(math.pow(vuongX - heartX,2)+math.pow(vuongY - heartY,2))
 
     if distance < 27:
         return True
@@ -100,69 +100,69 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                nganX_change = 1
+                thuyX_change = 1
             if event.key == pygame.K_LEFT:
-                nganX_change = -1
+                thuyX_change = -1
             if event.key == pygame.K_UP:
-                nganY_change = -1
+                thuyY_change = -1
             if event.key == pygame.K_DOWN:
-                nganY_change = 1
+                thuyY_change = 1
             if event.key == pygame.K_SPACE:
                 if heart_state == "ready":
-                    heartX = nganX
-                    heartY = nganY
+                    heartX = thuyX
+                    heartY = thuyY
                     heart(heartX,heartY)
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
-                nganX_change = 0
+                thuyX_change = 0
             if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                nganY_change = 0
+                thuyY_change = 0
 
-    ngan(nganX,nganY)
-    nganX += nganX_change
-    nganY += nganY_change
+    thuy(thuyX,thuyY)
+    thuyX += thuyX_change
+    thuyY += thuyY_change
 
-    if nganX <=0:
-        nganX = 0 
-    elif nganX >=736:
-        nganX = 736
+    if thuyX <=0:
+        thuyX = 0 
+    elif thuyX >=736:
+        thuyX = 736
 
-    if nganY <= 400: 
-        nganY = 400
-    elif nganY >=530:
-        nganY = 529
+    if thuyY <= 400: 
+        thuyY = 400
+    elif thuyY >=530:
+        thuyY = 529
 
-    for i in range(num_of_hai):
+    for i in range(num_of_vuong):
         # game over
-        if haiY[i] > 200:
-            for j in range(num_of_hai):
-                haiY[j] =2000
+        if vuongY[i] > 200:
+            for j in range(num_of_vuong):
+                vuongY[j] =2000
             game_over_text()
             break
 
-        hai(haiX[i],haiY[i],i)
+        vuong(vuongX[i],vuongY[i],i)
 
-        if haiX[i] <= 0:
-            haiX_change[i] = 0.2
-            haiY[i] += haiY_change[i]
-        if haiX[i] >= 736:
-            haiX_change[i] = -0.2
-            haiY[i] += haiY_change[i]
+        if vuongX[i] <= 0:
+            vuongX_change[i] = 0.2
+            vuongY[i] += vuongY_change[i]
+        if vuongX[i] >= 736:
+            vuongX_change[i] = -0.2
+            vuongY[i] += vuongY_change[i]
 
-        haiX[i] += haiX_change[i]
+        vuongX[i] += vuongX_change[i]
            
-        collision = iscollision(haiX[i],haiY[i],heartX,heartY)
+        collision = iscollision(vuongX[i],vuongY[i],heartX,heartY)
         if collision:
             heart_state = "ready"
-            haiY[i] = random.randint(50,150)
-            haiX[i] = random.randint(0,735)
+            vuongY[i] = random.randint(50,150)
+            vuongX[i] = random.randint(0,735)
             score_value += 1
             explosion_sound = mixer.Sound('tick.wav')
             explosion_sound.play()
 
     if heartY <= 0:
-        heartY = nganY
+        heartY = thuyY
         heart_state ="ready" 
     if heart_state == "fire":
         heartY -= heartY_change
